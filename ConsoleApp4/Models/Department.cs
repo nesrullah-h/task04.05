76765a9dafcb3c61faf5efaf98351d2cb2807c95
Nesrullah Hacizade <nesrullah.hacizade@gmail.com>
2022-04-05 09:57:51 +0400

    created task

---
+++ ConsoleApp4/Models/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4.Models
+{
+    internal class Department
+    {
+        private int _id;
+        public int Id { get; }
+        public List<Employee> _employees;
+        static void AddEmployee()
+        {
+
+        }
+    }
+}
---
+++ ConsoleApp4/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4.Models
+{
+    internal class Employee
+    {
+        private int _id;
+        public int Id { get; }
+        private string Name { get; set; }
+        private double Salary { get; set; }
+        static void ShowInfo()
+        {
+            
+        }
+    }
+}
